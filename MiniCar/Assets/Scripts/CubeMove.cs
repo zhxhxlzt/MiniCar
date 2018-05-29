4ec0a990c800a0e145dbd9651d4b9db163e1db6d
zhshxlzt <2284174996@qq.com>
2018-05-30 03:21:32 +0800

    changePlace

---
+++ MiniCar/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+	public int speed;
+	public Transform obj;
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
