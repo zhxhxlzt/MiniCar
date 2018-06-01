144386af2ab153a0309f5fb498f732900b9bbf9d
zhshxlzt <2284174996@qq.com>
2018-06-02 04:54:26 +0800

    构建赛车
    输入控制
    赛车控制
    相机控制

---
+++ MiniCar/Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [Header( "=== 控制对象 ===" )]
+    [SerializeField] private GameObject m_horizontal;
+    [SerializeField] private GameObject m_vertical;
+    [SerializeField] private GameObject m_camera;
+
+    [Header("=== 跟随目标 ===")]
+    [SerializeField]private GameObject m_target;
+
+    [Header( "=== 相机参数 ===" )]
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private bool m_isFollowing = true;
+    [SerializeField] private bool m_isLookAt = true;
+
+    private void Awake()
+    {
+        m_horizontal = GameObject.Find( "CamHor" );
+        m_vertical = GameObject.Find( "CamVer" );
+        m_camera = GameObject.Find( "Main Camera" );
+        m_target = GameObject.FindGameObjectWithTag( "Player" );
+    }
+
+    private void Start()
+    {
+        AdjustOffset();
+    }
+    private void FixedUpdate()
+    {
+        FollowTarget();
+        LookAtTarget();
+    }
+
+
+    private void FollowTarget()
+    {
+        if ( !m_isFollowing ) return;
+
+        transform.position = m_target.transform.position;
+        transform.forward = m_target.transform.forward;
+    }
+
+    private void LookAtTarget()
+    {
+        if ( !m_isLookAt ) return;
+
+        m_camera.transform.LookAt(m_target.transform);
+    }
+
+    private void AdjustOffset()
+    {
+        
+        m_horizontal.transform.position = transform.position + new Vector3( 0f, 0f, offset.z );
+        m_vertical.transform.position = m_horizontal.transform.position + new Vector3( 0f, offset.y, 0f );
+    }
+}
---
+++ MiniCar/Assets/Scripts/CarController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    [Header( "=== 外部控制器 ===" )]
+    [SerializeField] private InputHandler m_inputHandler;    //输入控制器
+
+    [Header( "=== 车轮模型 ===" )]
+    [SerializeField] private GameObject[] WheelModels;
+
+    [Header( "=== 车轮碰撞器 ===" )]
+    [SerializeField] private WheelCollider[] wheelColliders;
+
+    [Header( "=== 赛车属性 ===" )]
+    [SerializeField] private float m_maxSpeed;               //最高速度 m/s
+    [SerializeField] private float m_maxMotorTorque;        //最高动力扭矩
+    [SerializeField] private float m_maxSteer;              //最大转向角
+    [SerializeField] private float m_maxBrakeTorque;        //最大制动所知
+
+    [SerializeField] private bool m_isDriving = true;       //是否处于驾驶状态
+    [SerializeField] private bool m_isOnGround = true;      //是否着地
+    [SerializeField] private Vector3 m_centreOfMass = Vector3.zero;   //重心
+
+    private void Awake()
+    {
+        GetComponent<Rigidbody>().centerOfMass = m_centreOfMass;    //设置赛车重心位置
+    }
+
+    private void Start()
+    {
+        m_inputHandler = GameObject.FindGameObjectWithTag( "GameController" ).GetComponent<InputHandler>();
+    }
+
+    private void FixedUpdate()
+    {
+        DriveControl();
+        SyncWheels();
+    }
+
+    private void DriveControl()
+    {
+        //非驾驶状态不接受移动输入命令控制
+        if ( !m_isDriving )
+        {
+            return;
+        }
+
+        float motorTorque = m_maxMotorTorque / 4;   //各车轮动力扭矩
+        float brakeTorque = m_maxBrakeTorque / 4;   //各车轮制动扭矩
+
+        for( int i = 0; i < wheelColliders.Length; i++ )
+        {
+            wheelColliders[i].motorTorque = motorTorque * m_inputHandler.Accel; //动力
+            wheelColliders[i].brakeTorque = brakeTorque * m_inputHandler.Brake; //刹车
+        }
+
+        for( int i = 0; i < 2; i++ )
+        {
+            wheelColliders[i].steerAngle = m_maxSteer * m_inputHandler.Steer;   //转向
+        }
+
+    }
+
+    //同步车轮模型与碰撞器
+    private void SyncWheels()
+    {
+        for( int i = 0; i < wheelColliders.Length; i++ )
+        {
+            Vector3 pos;
+            Quaternion quat;
+
+            wheelColliders[i].GetWorldPose( out pos, out quat );        
+            WheelModels[i].transform.SetPositionAndRotation( pos, quat );
+        }
+    }
+}
